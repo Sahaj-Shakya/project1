e03f9c9db97d70984c187627bf7dee8e0b85656d
Sahaj-Shakya <shakyasahaj10@gmail.com>
2024-11-02 09:36:23 +0545

    news view add

--- app/news.php
+++ app/news.php
@@ -19,28 +19,30 @@ $result = mysqli_query($conn, $news_query);
     <?php include 'nav.php' ?>
 
 
-    <?php while ($row = mysqli_fetch_assoc($result)) {
-    $title = $row['title'];
-    $description = $row['description'];
-    $image = $row['image'];
-    echo "<div class='container container_news mb-3'>";
-        echo "<div class='container mb-2'>";
-            echo "<div class='row mt-4'>";
+    <?php while ($row = mysqli_fetch_assoc($result)): ?>
+        <?php
+        $sn = $row['sn'];
+        $title = $row['title'];
+        $description = $row['description'];
+        $image = $row['image'];
+        ?>
 
-                echo "<div class='container_img d-flex justify-content-center col-md-4 mb-3'>";
-                    echo "<img style='width: 100%; max-height: 230px; object-fit: cover;' src='$image' class='img-fluid rounded' alt='News Image'>";
-                echo "</div>";
+        <div class='container container_news mb-3'>
+            <div class='container mb-2'>
+                <div class='row mt-4'>
+                    <div class='container_img d-flex justify-content-center col-md-4 mb-3'>
+                        <img style='width: 100%; max-height: 230px; object-fit: cover;' src='<?php echo $image ?>' class='img-fluid rounded' alt='News Image'>
+                    </div>
+                    <div class='container_details col-md-8 mb-3'>
+                        <h4 style='display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical; overflow: hidden; text-overflow: ellipsis;'><?php echo $title ?></h4>
+                        <p style='display: -webkit-box; -webkit-line-clamp: 4; -webkit-box-orient: vertical; overflow: hidden; text-overflow: ellipsis;'><?php echo $description ?></p>
+                        <a href='news_view.php?sn=<?php echo $sn ?>' class='btn btn-outline-primary'>See more</a>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-                echo "<div class='container_details col-md-8 mb-3'>";
-                    echo "<h4>$title</h4>";
-                    echo "<p style='display: -webkit-box; -webkit-line-clamp: 4; -webkit-box-orient: vertical; overflow: hidden; text-overflow: ellipsis;'>$description</p>";
-                    echo "<a href='#' class='btn btn-outline-primary'>See more</a>";
-                echo "</div>";
-
-            echo "</div>";
-        echo "</div>";
-    echo "</div>";
-} ?>
+    <?php endwhile; ?>
 
     <?php include 'footer.php' ?>
 
---
+++ app/news_view.php
@@ -0,0 +1,47 @@
+<?php
+include 'connection.php';
+
+if(isset($_GET['sn'])){    
+    $sn = $_GET['sn'];
+}
+
+$news_query = "SELECT * FROM `news` WHERE `sn` = $sn LIMIT 1";
+$result = mysqli_query($conn, $news_query);
+
+$row = mysqli_fetch_assoc($result);
+$img = $row['image'];
+$title = $row['title'];
+$desc = $row['description'];
+
+?>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>News</title>
+</head>
+
+<body>
+
+    <?php include 'nav.php' ?>
+    <div class="container w-100 col-lg-8">
+        <div class="container container_left p-4 col-lg-9">
+            <div class="container mb-4">
+                <h2 style="text-align: center;"><?php echo $title ?></h2>
+            </div>
+            <div class="container d-flex justify-content-center">
+                <img src="<?php echo $img ?>" alt="img">
+            </div>
+            <div class="container">
+                <p><?php echo $desc ?></p>
+            </div>
+        </div>
+    </div>
+
+
+    <?php include 'footer.php' ?>
+</body>
+
+</html>
