6b15591e4ac62cf394bade3f38f365424112f380
Sahaj-Shakya <shakyasahaj10@gmail.com>
2024-11-09 21:37:23 +0545

    users.php initial commit

--- admin/users.php
+++ admin/users.php
@@ -1,9 +1,21 @@
-session_start();
-$admin = isset($_SESSION['admin']) ? $_SESSION['admin'] : ''; 
+<?php
 
-if($admin === ''){
+session_start();
+$admin = isset($_SESSION['admin']) ? $_SESSION['admin'] : '';
+
+if ($admin === '') {
     header('Location: login.php');
 }
+
+include '../app/connection.php';
+
+$users_query = "SELECT * FROM `user`";
+$users_result = mysqli_query($conn, $users_query);
+
+$superuser_query = "SELECT * FROM `admin`";
+$superuser_result = mysqli_query($conn, $superuser_query);
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -21,20 +33,85 @@ if($admin === ''){
 
 
         <div class="right-side">
-            <nav aria-label">
-                <ol class="breadcrumb mt-4 ms-3">
-                    <!-- <li class="breadcrumb-item"><a href="#">Home</a></li> -->
-                    <li class="breadcrumb-item"><a href="dashboard.php">Admin</a></li>
-                    <li class="breadcrumb-item active" aria-current="page">Users</li>
-                </ol>
-            </nav>
+            <div class="bread d-flex justify-content-between">
+                <nav aria-label">
+                    <ol class="breadcrumb mt-4 ms-3">
+                        <!-- <li class="breadcrumb-item"><a href="#">Home</a></li> -->
+                        <li class="breadcrumb-item"><a href="dashboard.php">Admin</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Schedules</li>
+                    </ol>
+                </nav>
+                <div class="add mt-4 me-3">
+                    <a href="schedules_add.php" class="btn btn-primary">Add Schedules</a>
+                </div>
+            </div>
+
+            <?php if (isset($_SESSION['admin_message'])): ?>
+                <div class="container mt-2">
+                    <div class="row justify-content-center">
+                        <div class="col-12 col-sm-10 col-md-8 col-lg-6">
+                            <div class="alert alert-secondary alert-dismissible fade show" role="alert">
+                                <?php echo $_SESSION['admin_message']; ?>
+                                <?php unset($_SESSION['admin_message']); ?>
+                                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php endif; ?>
+
+            <div class="container border p-2 mt-5" style="border-radius: 10px; max-width: 900px;">
+                <div class="container p-3">
+                    <h4 class="text-center">Users</h4>
+                    <hr>
+                    <?php while ($row = mysqli_fetch_assoc($users_result)): ?>
+                        <?php
+                        $sn = $row['sn'];
+                        $username = $row['username'];
+                        // $title = $row['email'];
+                        ?>
+                        <ul class="list-group">
+                            <li class="list-group-item d-flex justify-content-between d-flex align-items-center">
+                                <p style="max-width: 840px; font-size: large;"><?php echo $username ?></p>
+                                <div class="actions">
+                                    <a href="schedules_edit.php?sn=<?php echo $sn; ?>" class="btn btn-warning">Edit</a>
+                                    <a href="schedules_delete.php?sn=<?php echo $sn; ?>" class="btn btn-danger">Delete</a>
+                                </div>
+                            </li>
+                        </ul>
+                    <?php endwhile; ?>
+                </div>
+            </div>
+
+            <div class="container border p-2 mt-5" style="border-radius: 10px; max-width: 900px;">
+                <div class="container p-3">
+                    <h4 class="text-center">Super Users</h4>
+                    <hr>
+                    <?php while ($row = mysqli_fetch_assoc($superuser_result)): ?>
+                        <?php
+                        $sn = $row['sn'];
+                        $username = $row['name'];
+                        // $title = $row['email'];
+                        ?>
+                        <ul class="list-group">
+                            <li class="list-group-item d-flex justify-content-between d-flex align-items-center">
+                                <p style="max-width: 840px; font-size: large;"><?php echo $username ?></p>
+                                <div class="actions">
+                                    <a href="schedules_edit.php?sn=<?php echo $sn; ?>" class="btn btn-warning">Edit</a>
+                                    <a href="schedules_delete.php?sn=<?php echo $sn; ?>" class="btn btn-danger">Delete</a>
+                                </div>
+                            </li>
+                        </ul>
+                    <?php endwhile; ?>
+                </div>
+            </div>
         </div>
     </div>
 
     <?php include "sidebar.php" ?>
 
 
-    
+
 
 </body>
 
